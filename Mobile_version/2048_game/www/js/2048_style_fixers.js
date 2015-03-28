04426dac7ba79dced1217ddf3428238bf5ec511f
Abidul <abd_rmdn@yahoo.com>
2015-03-29 05:25:04 +0800

    working now with swipes and style hazy

---
+++ Mobile_version/2048_game/www/js/2048_style_fixers.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+//to keep the size boxed
+$('.Game_Block').height($('.Game_Block').width());
+
+});
--- Mobile_version/README.md
+++
@@ -1,2 +0,0 @@
-# 2048-Game
-a web nice version of the Known Game 2048
