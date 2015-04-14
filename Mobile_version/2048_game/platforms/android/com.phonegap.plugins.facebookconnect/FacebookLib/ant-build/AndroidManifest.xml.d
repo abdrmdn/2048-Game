79f9a2f7ea009d6a3f836048e307bdc5e0ab696d
Abidul <abd_rmdn@yahoo.com>
2015-04-14 22:59:48 +0800

    all good

---
+++ Mobile_version/2048_game/platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/abidul/www/2048-Game/Mobile_version/2048_game/platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/ant-build/AndroidManifest.xml : \
+/Users/abidul/www/2048-Game/Mobile_version/2048_game/platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/AndroidManifest.xml \
